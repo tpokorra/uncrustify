75cbf0f295fe2465537a124f58432962e9d0b7b3
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2016-04-23 08:13:44 +0200

    C# 6: add a test for string interpolation

---
+++ tests/input/cs/string_interpolation.cs
@@ -0,0 +1,7 @@
+public class C
+{
+public string Name = "John";
+public string LastName = "Doe";
+// Test of new C# 6 'string interpolation' feature, $ sign
+public string FullName => $"Hello {Name} {LastName}";
+}
