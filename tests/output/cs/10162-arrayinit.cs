1acaa96506eed9c9f115aeb2a1beaf9f2ac33500
Timotheus Pokorra <tp@tbits.net>
2017-06-08 07:44:19 +0200

    add unit test for issue with csharp/ICT/Common/Data/TypedDataTable.cs

---
+++ tests/output/cs/10162-arrayinit.cs
@@ -0,0 +1,17 @@
+class Test
+{
+   /// constructor1
+   public Test(short AId, int A2)
+      : this(AId, A2)
+   {
+   }
+
+   /// constructor2
+   public Test(short AId)
+      : this(AId, new int[] {})
+   {
+      Console.WriteLine(AId.ToString());
+
+      foo(new int[] {});
+   }
+}
